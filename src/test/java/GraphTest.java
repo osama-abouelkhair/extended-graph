4b8325cf749b0aa0a80c61959c9238c381a2a640
osama abo elkhair <osama.abulkhair@gmail.com>
2019-11-17 21:40:26 +0100

    add-unit-test-for-Graph

---
+++ src/test/java/GraphTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void depthFirstSearch() {
+    }
+
+    @Test
+    void hasPathTo() {
+    }
+
+    @Test
+    void breadthFirstSearch() {
+    }
+
+    @Test
+    void getEdges() {
+    }
+}
